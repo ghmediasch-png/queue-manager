94a0f7456df729ebf63078f5e17942a53503f78b
odark007 <gofrance01@gmail.com>
2025-11-26 20:55:28 +0000

    Config: Added basePath for subdirectory deployment

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  basePath: '/queue',
+  // This ensures assets (images/css) load correctly from the subdirectory
+  assetPrefix: '/queue', 
 };
 
 export default nextConfig;
